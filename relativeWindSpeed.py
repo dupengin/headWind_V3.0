1195535bb6e84ac0300bedec09dfe1a996530812
dupengin <pearceduffy1@gmail.com>
2025-07-07 20:08:44 +0000

    first file commit

---
+++ relativeWindSpeed.py
@@ -0,0 +1,12 @@
+"""This module calculates the relative wind speed to the gpx file data
+
+Relative wind speed 
+
+Methodogy 
+- Calculate the North South component and East West components for wind and gpx data
+- Find resultant in N/S and E/W by summing 
+- Use trig to find resultant wind speed 
+
+"""
+
+def relativeWindSpeed(data_in):
