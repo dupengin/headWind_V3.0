d14e5bae4439d9a33ccda56a9e40a365b73ecd19
dupengin <pearceduffy1@gmail.com>
2025-07-10 15:20:54 +0000

    code use visual crossing API

--- windSpeed.py
+++ windSpeed.py
@@ -10,6 +10,8 @@ def windSpeed(gpx_data, test_mode, test_file):
     from dotenv import load_dotenv
     import os
     import csv
+    import datetime
+    import requests
 
     api_key = os.getenv("API_KEY")
     wind_csv = []
@@ -18,6 +20,7 @@ def windSpeed(gpx_data, test_mode, test_file):
 
     if test_mode : #use test data and don't make api call
         #wind_data = open(test_file, 'r')
+        print ("in test mode")
  
         #wind dir in column 0 and wind speed in column 1
         with open(test_file) as f:         
@@ -29,14 +32,38 @@ def windSpeed(gpx_data, test_mode, test_file):
                
 
         for i in range(len(gpx_data)):
-            gpx_data[i]["wind_speed"] = wind_speed
-            gpx_data[i]["wind_bearing"] = wind_bearing
+            gpx_data[i]["wind_speed"] = int(wind_speed)
+            gpx_data[i]["wind_bearing"] = int(wind_bearing)
         
         all_data = gpx_data
             
         
 
     else: #make api call to get weather data
-        print('api call')
+        
+        for i in range (len(gpx_data)):
+            lat = gpx_data[i]['lat'] 
+            lon = gpx_data[i]['longit'] 
+            time = gpx_data[i]['time'] #convert to UNIX time
+            date = gpx_data[i]['date']
+            dt = datetime.datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M:%S")
+            unix_t = int(dt.replace(tzinfo=datetime.timezone.utc).timestamp())
+
+            try:
+                response = requests.get((f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2024-12-15T13:00:00?key=KWRSQR9EWW93RWEUXGXED6WSL"),
+                timeout=10)
+                
+                response.raise_for_status() # Raises HTTPError for bad responses (4xx or 5xx)
+
+                
+            except requests.exceptions.HTTPError as http_err:
+                print(f"HTTP error occurred: {http_err} - Status code: {response.status_code}")
+            except requests.exceptions.ConnectionError:
+                print("Error connecting to the API.")
+            except requests.exceptions.Timeout:
+                print("The request timed out.")
+            except requests.exceptions.RequestException as err:
+                print(f"An error occurred: {err}")
+
 
     return all_data
